66c4fab26743e7e801fbe043c151d3988b1a1849
Enor Cerna <u17200479@utp.edu.pe>
2024-11-09 02:30:44 -0500

    add route main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import {Hono} from "hono";
 import {PORT} from "@/constants/main.ts";
+import v1 from "@/routes/v1/index.ts";
 const app = new Hono();
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+//routes
+app.route("", v1);
 
 Deno.serve({port: PORT}, app.fetch);
