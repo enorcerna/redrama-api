8ed1ff8ac0f27ede5fd0f2340657f4dc33101b05
Enor Cerna <u17200479@utp.edu.pe>
2024-11-09 02:38:37 -0500

    add labels

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import {logger} from "hono/logger";
 import {Hono} from "hono";
 import {PORT} from "@/constants/main.ts";
 import v1 from "@/routes/v1/index.ts";
+import labels from "@/routes/v1/label/index.ts";
 const app = new Hono();
 // middleware
 app.use(logger());
 //routes
 app.route("", v1);
+app.route("", labels);
 
 Deno.serve({port: PORT}, app.fetch);
