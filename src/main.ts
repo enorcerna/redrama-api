9abd443d162aa7742ab01ead47b68f5969d858fc
Enor Cerna <u17200479@utp.edu.pe>
2024-11-09 02:32:52 -0500

    add middleware logger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+import {logger} from "hono/logger";
 import {Hono} from "hono";
 import {PORT} from "@/constants/main.ts";
 import v1 from "@/routes/v1/index.ts";
 const app = new Hono();
+// middleware
+app.use(logger());
 //routes
 app.route("", v1);
 
