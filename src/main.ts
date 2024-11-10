3cb382b91b352e788b49c32fde119544ea0588e7
Enor Cerna <u17200479@utp.edu.pe>
2024-11-10 04:29:18 -0500

    add route search

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {PORT} from "@/constants/main.ts";
 import v1 from "@/routes/v1/index.ts";
 import labels from "@/routes/v1/label/index.ts";
 import countries from "@/routes/v1/country/index.ts";
+import search from "@/routes/v1/search/index.ts";
 const app = new Hono();
 // middleware
 app.use(logger());
@@ -11,5 +12,6 @@ app.use(logger());
 app.route("", v1);
 app.route("", labels);
 app.route("", countries);
+app.route("", search);
 
 Deno.serve({port: PORT}, app.fetch);
