35a70ee36f71e582bf08c05fae3402227589326b
Enor Cerna <u17200479@utp.edu.pe>
2024-11-10 00:48:59 -0500

    add var

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import {RequestDocument, Variables} from "@graphql/types";
 import {GraphQLClient} from "@graphql";
-import {labelQuery} from "@/gql/index.ts";
+import {labelQuery, countryQuery} from "@/gql/index.ts";
 class Api {
   private api_url: string;
   private client: GraphQLClient;
@@ -18,6 +18,9 @@ class Api {
   // label
   async getLabels() {
     return await this.Request(labelQuery, {});
+  }
+  async getCountries() {
+    return await this.Request(countryQuery, {platform: "doramasgo"});
   }
 }
 export const ApiClient = new Api();
