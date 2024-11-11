77c74bcbc38458d0a8484c2b0b2d5a87ccd8748e
Enor Cerna <u17200479@utp.edu.pe>
2024-11-10 23:07:16 -0500

    add route last home

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import v1 from "@/routes/v1/index.ts";
 import labels from "@/routes/v1/label/index.ts";
 import countries from "@/routes/v1/country/index.ts";
 import search from "@/routes/v1/search/index.ts";
+import home from "@/routes/v1/home/index.ts";
 const app = new Hono();
 // middleware
 app.use(logger(), cors());
@@ -14,5 +15,6 @@ app.route("", v1);
 app.route("", labels);
 app.route("", countries);
 app.route("", search);
+app.route("", home);
 
 Deno.serve({port: PORT}, app.fetch);
