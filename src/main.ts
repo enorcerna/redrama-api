7d836e1f52efda0ce15c96bd7d0854812fbfa51e
Enor Cerna <u17200479@utp.edu.pe>
2024-11-10 23:05:54 -0500

    cors middleware

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import {logger} from "hono/logger";
+import {cors} from "hono/cors";
 import {Hono} from "hono";
 import {PORT} from "@/constants/main.ts";
 import v1 from "@/routes/v1/index.ts";
@@ -7,7 +8,7 @@ import countries from "@/routes/v1/country/index.ts";
 import search from "@/routes/v1/search/index.ts";
 const app = new Hono();
 // middleware
-app.use(logger());
+app.use(logger(), cors());
 //routes
 app.route("", v1);
 app.route("", labels);
