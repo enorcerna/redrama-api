9b386bb35e72bfa7a0f1396b0194f6277cbd5b24
Enor Cerna <u17200479@utp.edu.pe>
2024-11-12 05:02:57 -0500

    gnre & episode & drama

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,22 @@ import {GraphQLClient} from "@graphql";
 import {labelQuery, countryQuery} from "@/gql/index.ts";
 import {searchQuery} from "@/gql/searchQuery.ts";
 import {lastListDramaQuery} from "@/gql/LastListDramaQuery.ts";
+import {
+  getDramaBySlugQuery,
+  getDramaEpisodesByIdQuery,
+  getDramasSimilarByIdSQuery
+} from "@/gql/dramaQuery.ts";
+import {
+  getEpisodeLinksByIdQuery,
+  getEpisodeNextByIdQuery,
+  getEpisodePrevByIdQuery
+} from "@/gql/episodeQuery.ts";
+import {getEpisodeBySlugQuery} from "@/gql/episodeQuery.ts";
+import {
+  getGenreBySlugQuery,
+  getListGenreQuery,
+  getPaginationDramasByGenreIdQuery
+} from "@/gql/genreQuery.ts";
 class Api {
   private api_url: string;
   private client: GraphQLClient;
@@ -33,6 +49,55 @@ class Api {
       limit: 15,
       filter: {isTVShow: false}
     });
+  }
+  // drama
+  async getDramaBySlug(slug: string) {
+    return await this.Request(getDramaBySlugQuery, {slug});
+  }
+  async getDramasSimilarById(id: string) {
+    return await this.Request(getDramasSimilarByIdSQuery, {dorama_id: id});
+  }
+  async getDramaEpisodesById(id: string) {
+    return await this.Request(getDramaEpisodesByIdQuery, {
+      page: 1,
+      perPage: 16,
+      serie_id: String(id),
+      season_number: 1
+    });
+  }
+  // episodes
+  async getEpisodesBySlug(slug: string) {
+    return await this.Request(getEpisodeBySlugQuery, {slug});
+  }
+  async getEpisodeLinksById(id: string) {
+    return await this.Request(getEpisodeLinksByIdQuery, {
+      id: String(id),
+      app: "com.asiapp.doramasgo"
+    });
+  }
+  async getEpisodeNextById(id: string) {
+    return await this.Request(getEpisodeNextByIdQuery, {episode_id: id});
+  }
+  async getEpisodePrevById(id: string) {
+    return await this.Request(getEpisodePrevByIdQuery, {episode_id: id});
+  }
+  // genre
+  async getListGenre() {
+    const lt = await this.Request(getListGenreQuery, {});
+    return lt.listGenres;
+  }
+  async getGenreBySlug(slug: string) {
+    const g = await this.Request(getGenreBySlugQuery, {slug});
+    return g.detailGenre;
+  }
+  async getPaginationDramasByGenreId(id: string, page = 1) {
+    const p = await this.Request(getPaginationDramasByGenreIdQuery, {
+      page,
+      sort: "CREATEDAT_DESC",
+      perPage: 20,
+      filter: {genreId: id, isTVShow: false}
+    });
+    return p.paginationDorama;
   }
 }
 export const ApiClient = new Api();
